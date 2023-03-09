7c53a191c82a189abc375193b520488abc48fc48
DGgithubDG <thenumerousrandom@gmail.com>
2023-03-09 15:50:21 +0000

    trsting

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,46 @@
 import React from "react";
+//import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+import { FcApproval } from "react-icons/fc";
+import { FcMoneyTransfer } from "react-icons/fc";
+import { FcLink } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import casino from "../../img/casino.9.jpg";
+import Navbar from "./navbar";
+import Footer from "./footer";
+//import Card from "./card";
+//import ResponsiveImage from "./img";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+        <div className="Background">
+    <Navbar/>
+    <div></div><div>
+    <img class="img-fluid" src={casino}></img>
+    <div class="card text-center">
+  <div class="card-body">
+    <h5 class="card-title"><h1>PEGASINO</h1></h5>
+
+    <p class="card-text text-start"><FcApproval size={38}/><strong>Always the best odds with Casino</strong></p>
+    <p class="card-text text-start"><FcMoneyTransfer size={38}/><strong>Quick and easy deposits & withdrawals</strong></p>
+    <p class="card-text text-start"><FcLink size={38}/><strong>Transparency, responsibility and security</strong></p>
+   
+    <a href="#" class="button btn btn-success">REGISTER</a>
+    {/* <Link to="/">
+				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+			</Link> */}
+  </div>
+</div>
+    {/* <img className="Back" src="https://www.wildz.com/cdn/2021/09/30/wildz-blog-online-casino-2_uid_6155c3688c52d.jpeg?&cover=true" class="img-fluid" alt=""/>  */}
+     {/* <Card/>  */}
+    </div>
+    <Footer/>
+    </div>
+
+    
+    
 	);
 };
 
---
+++ src/js/component/navbar.js
@@ -0,0 +1,65 @@
+import React, {useState }from "react";
+//import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+import { FaHome } from 'react-icons/fa'
+import { FaBasketballBall } from 'react-icons/fa'
+import { FaGamepad } from 'react-icons/fa'
+import { FaMoneyBillAlt } from 'react-icons/fa'
+import { GiCard7Clubs } from 'react-icons/gi'
+import { MdCasino, MdStars } from 'react-icons/md'
+import { ImNewspaper } from 'react-icons/im'
+
+
+
+
+
+//create your first component
+const Navbar = () => {
+	return (
+<div>
+		<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><h1><strong>PEGASINO</strong></h1></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+        <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>SPORTS</i></button><FaBasketballBall/></div></a>
+        </li>
+		<li class="nav-item">
+    <a className="sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>CASINO</i></button><FaMoneyBillAlt/></div></a>
+        </li>
+		<li class="nav-item">
+        <a className="sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>LIVE CASINO</i></button><MdCasino/></div></a>
+        </li>
+		<li class="nav-item">
+    <a className="sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>VIRTUAL GAMES</i></button><FaGamepad/></div></a>
+        </li>
+		<li class="nav-item">
+    <a className="sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>POKER</i></button><GiCard7Clubs/></div></a>
+        </li>
+		<li class="nav-item">
+    <a className="sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>PROMOTION</i></button><MdStars/></div></a>
+        </li>
+		<li class="nav-item">
+    <a className="sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>BLOG</i></button><ImNewspaper/></div></a>
+        </li>
+		
+      </ul>
+      
+      <form className="d-flex" role="search">
+		<div className="Register"><button type="button" className="btn btn-success">REGISTER</button></div>
+		<div className="Login"><button type="button" className="btn btn-success">LOGIN</button></div>
+
+
+      </form>
+    </div>
+  </div>
+</nav>
+	</div>
+
+	);
+};
+
+export default Navbar;
