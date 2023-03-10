1c9bf2724fbe17ac59c0784140a1509af5586f96
DGgithubDG <thenumerousrandom@gmail.com>
2023-03-10 11:42:02 +0000

    w

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,7 +3,11 @@ import React from "react";
 import { FcApproval } from "react-icons/fc";
 import { FcMoneyTransfer } from "react-icons/fc";
 import { FcLink } from "react-icons/fc";
-import { Link } from "react-router-dom";
+//import { MDBIcon } from 'mdb-react-ui-kit';
+//import { BG } from 'country-flag-icons/react/3x2'
+
+
+//import { Link } from "react-router-dom";
 
 import casino from "../../img/casino.9.jpg";
 import Navbar from "./navbar";
@@ -22,12 +26,15 @@ const Home = () => {
     <div class="card text-center">
   <div class="card-body">
     <h5 class="card-title"><h1>PEGASINO</h1></h5>
+    {/* <BG title="Bulgaria" className="falg"/> */}
+
 
     <p class="card-text text-start"><FcApproval size={38}/><strong>Always the best odds with Casino</strong></p>
     <p class="card-text text-start"><FcMoneyTransfer size={38}/><strong>Quick and easy deposits & withdrawals</strong></p>
     <p class="card-text text-start"><FcLink size={38}/><strong>Transparency, responsibility and security</strong></p>
    
     <a href="#" class="button btn btn-success">REGISTER</a>
+  
     {/* <Link to="/">
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 			</Link> */}
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -7,6 +7,7 @@ import { FaMoneyBillAlt } from 'react-icons/fa'
 import { GiCard7Clubs } from 'react-icons/gi'
 import { MdCasino, MdStars } from 'react-icons/md'
 import { ImNewspaper } from 'react-icons/im'
+import { BG } from 'country-flag-icons/react/3x2'
 
 
 
@@ -15,50 +16,46 @@ import { ImNewspaper } from 'react-icons/im'
 //create your first component
 const Navbar = () => {
 	return (
-<div>
-		<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><h1><strong>PEGASINO</strong></h1></a>
+  <a class="navbar-brand" href="#"><h1><strong>PEGASINO</strong></h1></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
+	  <li class="nav-item">
         <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>SPORTS</i></button><FaBasketballBall/></div></a>
         </li>
-		<li class="nav-item">
-    <a className="sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>CASINO</i></button><FaMoneyBillAlt/></div></a>
+        <li class="nav-item">
+        <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>CASINO</i></button><MdCasino/></div></a>
         </li>
+        <li class="nav-item dropdown">
 		<li class="nav-item">
-        <a className="sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>LIVE CASINO</i></button><MdCasino/></div></a>
+        <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>LIVE CASINO</i></button><FaMoneyBillAlt/></div></a>
         </li>
-		<li class="nav-item">
-    <a className="sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>VIRTUAL GAMES</i></button><FaGamepad/></div></a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+		  <li class="nav-item">
+        <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>POKER</i></button><GiCard7Clubs/></div></a>
         </li>
-		<li class="nav-item">
-    <a className="sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>POKER</i></button><GiCard7Clubs/></div></a>
+			     </ul>
         </li>
-		<li class="nav-item">
-    <a className="sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>PROMOTION</i></button><MdStars/></div></a>
-        </li>
-		<li class="nav-item">
-    <a className="sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>BLOG</i></button><ImNewspaper/></div></a>
+        <li class="nav-item">
+        <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>VIRTUAL GAMES</i></button><FaGamepad/></div></a>
         </li>
 		
+		
+		<li class="nav-item">
+        <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>BLOG</i></button><ImNewspaper/></div></a>
+        </li>
       </ul>
-      
-      <form className="d-flex" role="search">
-		<div className="Register"><button type="button" className="btn btn-success">REGISTER</button></div>
-		<div className="Login"><button type="button" className="btn btn-success">LOGIN</button></div>
-
-
+      <form class="d-flex">
+	  <div className="Login"><button type="button" className="btn btn-success">REGISTER</button></div>
+	  <div className="Register"><button type="button" className="btn btn-success">LOGIN</button></div>
       </form>
     </div>
   </div>
 </nav>
-	</div>
-
 	);
 };
 
